5d06ab1008f18e804932091684f092b38e11366f
Murad Bashirov <carlsonmu@gmail.com>
2023-10-21 18:07:41 +0900

    handle path

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,29 @@
-use std::io::prelude::*;
+use std::io::{prelude::*, BufReader};
 use std::net::TcpListener;
 
-fn main() -> anyhow::Result<()> {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+use anyhow::Context;
 
-    // Uncomment this block to pass the first stage
-    //
+fn main() -> anyhow::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:4221").expect("sockaddr is correct");
 
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                println!("connection received");
-                stream.write(b"HTTP/1.1 200 OK\r\n\r\n")?;
+                let mut reader = BufReader::new(&mut stream);
+                let mut start_line = String::new();
+                reader.read_line(&mut start_line)?;
+                let path = start_line
+                    .split_whitespace()
+                    .nth(1)
+                    .expect("start line have path in it");
+                match path {
+                    "/" => {
+                        stream.write(b"HTTP/1.1 200 OK\r\n\r\n")?;
+                    }
+                    _ => {
+                        stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n")?;
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);

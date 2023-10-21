e07e95061c531e7c2305bf513347fed887ed32a4
Murad Bashirov <carlsonmu@gmail.com>
2023-10-21 17:53:59 +0900

    respond 200

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,24 @@
-// Uncomment this block to pass the first stage
+use std::io::prelude::*;
 use std::net::TcpListener;
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
     //
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:4221").expect("sockaddr is correct");
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                println!("connection received");
+                stream.write(b"HTTP/1.1 200 OK\r\n\r\n")?;
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+    Ok(())
 }
